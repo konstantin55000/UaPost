12a816a0371552c27647de5ad5fda06a1efad83d
konstantin55000 <pkonstantin55000@gmail.com>
2018-02-17 20:16:17 +0200

    changed country; clear code

--- fillAddress.js
+++ fillAddress.js
@@ -3,6 +3,253 @@
 
 //})();
 
+var countryList = [ 
+  {name: 'Afghanistan', code: 'AF'}, 
+  {name: 'Åland Islands', code: 'AX'}, 
+  {name: 'Abkhazia', code: 'AB'},   	
+  {name: 'Albania', code: 'AL'}, 
+  {name: 'Algeria', code: 'DZ'}, 
+  {name: 'American Samoa', code: 'AS'}, 
+  {name: 'AndorrA', code: 'AD'}, 
+  {name: 'Angola', code: 'AO'}, 
+  {name: 'Anguilla', code: 'AI'}, 
+  {name: 'Antarctica', code: 'AQ'}, 
+  {name: 'Antigua and Barbuda', code: 'AG'}, 
+  {name: 'Argentina', code: 'AR'}, 
+  {name: 'Armenia', code: 'AM'}, 
+  {name: 'Aruba', code: 'AW'}, 
+  {name: 'Australia', code: 'AU'}, 
+  {name: 'Austria', code: 'AT'}, 
+  {name: 'Azerbaijan', code: 'AZ'}, 
+  {name: 'Bahamas', code: 'BS'}, 
+  {name: 'Bahrain', code: 'BH'}, 
+  {name: 'Bangladesh', code: 'BD'}, 
+  {name: 'Barbados', code: 'BB'}, 
+  {name: 'Belarus', code: 'BY'}, 
+  {name: 'Belgium', code: 'BE'}, 
+  {name: 'Belize', code: 'BZ'}, 
+  {name: 'Benin', code: 'BJ'}, 
+  {name: 'Bermuda', code: 'BM'}, 
+  {name: 'Bhutan', code: 'BT'}, 
+  {name: 'Bolivia', code: 'BO'}, 
+  {name: 'Bosnia and Herzegovina', code: 'BA'}, 
+  {name: 'Botswana', code: 'BW'}, 
+  {name: 'Bouvet Island', code: 'BV'}, 
+  {name: 'Brazil', code: 'BR'}, 
+  {name: 'British Indian Ocean Territory', code: 'IO'}, 
+  {name: 'Brunei Darussalam', code: 'BN'}, 
+  {name: 'Bulgaria', code: 'BG'}, 
+  {name: 'Burkina Faso', code: 'BF'}, 
+  {name: 'Burundi', code: 'BI'}, 
+  {name: 'Cambodia', code: 'KH'}, 
+  {name: 'Cameroon', code: 'CM'}, 
+  {name: 'Canada', code: 'CA'}, 
+  {name: 'Cape Verde', code: 'CV'}, 
+  {name: 'Cayman Islands', code: 'KY'}, 
+  {name: 'Central African Republic', code: 'CF'}, 
+  {name: 'Chad', code: 'TD'}, 
+  {name: 'Chile', code: 'CL'}, 
+  {name: 'China', code: 'CN'}, 
+  {name: 'Christmas Island', code: 'CX'}, 
+  {name: 'Cocos (Keeling) Islands', code: 'CC'}, 
+  {name: 'Colombia', code: 'CO'}, 
+  {name: 'Comoros', code: 'KM'}, 
+  {name: 'Congo', code: 'CG'}, 
+  {name: 'Congo, The Democratic Republic of the', code: 'CD'}, 
+  {name: 'Cook Islands', code: 'CK'}, 
+  {name: 'Costa Rica', code: 'CR'}, 
+  {name: 'Cote D\'Ivoire', code: 'CI'}, 
+  {name: 'Croatia', code: 'HR'}, 
+  {name: 'Cuba', code: 'CU'}, 
+  {name: 'Cyprus', code: 'CY'}, 
+  {name: 'Czech Republic', code: 'CZ'}, 
+  {name: 'Denmark', code: 'DK'}, 
+  {name: 'Djibouti', code: 'DJ'}, 
+  {name: 'Dominica', code: 'DM'}, 
+  {name: 'Dominican Republic', code: 'DO'}, 
+  {name: 'Ecuador', code: 'EC'}, 
+  {name: 'Egypt', code: 'EG'}, 
+  {name: 'El Salvador', code: 'SV'}, 
+  {name: 'Equatorial Guinea', code: 'GQ'}, 
+  {name: 'Eritrea', code: 'ER'}, 
+  {name: 'Estonia', code: 'EE'}, 
+  {name: 'Ethiopia', code: 'ET'}, 
+  {name: 'Falkland Islands (Malvinas)', code: 'FK'}, 
+  {name: 'Faroe Islands', code: 'FO'}, 
+  {name: 'Fiji', code: 'FJ'}, 
+  {name: 'Finland', code: 'FI'}, 
+  {name: 'France', code: 'FR'}, 
+  {name: 'French Guiana', code: 'GF'}, 
+  {name: 'French Polynesia', code: 'PF'}, 
+  {name: 'French Southern Territories', code: 'TF'}, 
+  {name: 'Gabon', code: 'GA'}, 
+  {name: 'Gambia', code: 'GM'}, 
+  {name: 'Georgia', code: 'GE'}, 
+  {name: 'Germany', code: 'DE'}, 
+  {name: 'Ghana', code: 'GH'}, 
+  {name: 'Gibraltar', code: 'GI'}, 
+  {name: 'Greece', code: 'GR'}, 
+  {name: 'Greenland', code: 'GL'}, 
+  {name: 'Grenada', code: 'GD'}, 
+  {name: 'Guadeloupe', code: 'GP'}, 
+  {name: 'Guam', code: 'GU'}, 
+  {name: 'Guatemala', code: 'GT'}, 
+  {name: 'Guernsey', code: 'GG'}, 
+  {name: 'Guinea', code: 'GN'}, 
+  {name: 'Guinea-Bissau', code: 'GW'}, 
+  {name: 'Guyana', code: 'GY'}, 
+  {name: 'Haiti', code: 'HT'}, 
+  {name: 'Heard Island and Mcdonald Islands', code: 'HM'}, 
+  {name: 'Holy See (Vatican City State)', code: 'VA'}, 
+  {name: 'Honduras', code: 'HN'}, 
+  {name: 'Hong Kong', code: 'HK'}, 
+  {name: 'Hungary', code: 'HU'}, 
+  {name: 'Iceland', code: 'IS'}, 
+  {name: 'India', code: 'IN'}, 
+  {name: 'Indonesia', code: 'ID'}, 
+  {name: 'Iran, Islamic Republic Of', code: 'IR'}, 
+  {name: 'Iraq', code: 'IQ'}, 
+  {name: 'Ireland', code: 'IE'}, 
+  {name: 'Isle of Man', code: 'IM'}, 
+  {name: 'Israel', code: 'IL'}, 
+  {name: 'Italy', code: 'IT'}, 
+  {name: 'Jamaica', code: 'JM'}, 
+  {name: 'Japan', code: 'JP'}, 
+  {name: 'Jersey', code: 'JE'}, 
+  {name: 'Jordan', code: 'JO'}, 
+  {name: 'Kazakhstan', code: 'KZ'}, 
+  {name: 'Kenya', code: 'KE'}, 
+  {name: 'Kiribati', code: 'KI'}, 
+  {name: 'Korea, Democratic People\'S Republic of', code: 'KP'}, 
+  {name: 'Korea, Republic of', code: 'KR'}, 
+  {name: 'Kuwait', code: 'KW'}, 
+  {name: 'Kyrgyzstan', code: 'KG'}, 
+  {name: 'Lao People\'S Democratic Republic', code: 'LA'}, 
+  {name: 'Latvia', code: 'LV'}, 
+  {name: 'Lebanon', code: 'LB'}, 
+  {name: 'Lesotho', code: 'LS'}, 
+  {name: 'Liberia', code: 'LR'}, 
+  {name: 'Libyan Arab Jamahiriya', code: 'LY'}, 
+  {name: 'Liechtenstein', code: 'LI'}, 
+  {name: 'Lithuania', code: 'LT'}, 
+  {name: 'Luxembourg', code: 'LU'}, 
+  {name: 'Macao', code: 'MO'}, 
+  {name: 'Macedonia, The Former Yugoslav Republic of', code: 'MK'}, 
+  {name: 'Madagascar', code: 'MG'}, 
+  {name: 'Malawi', code: 'MW'}, 
+  {name: 'Malaysia', code: 'MY'}, 
+  {name: 'Maldives', code: 'MV'}, 
+  {name: 'Mali', code: 'ML'}, 
+  {name: 'Malta', code: 'MT'}, 
+  {name: 'Marshall Islands', code: 'MH'}, 
+  {name: 'Martinique', code: 'MQ'}, 
+  {name: 'Mauritania', code: 'MR'}, 
+  {name: 'Mauritius', code: 'MU'}, 
+  {name: 'Mayotte', code: 'YT'}, 
+  {name: 'Mexico', code: 'MX'}, 
+  {name: 'Micronesia, Federated States of', code: 'FM'}, 
+  {name: 'Moldova, Republic of', code: 'MD'}, 
+  {name: 'Monaco', code: 'MC'}, 
+  {name: 'Mongolia', code: 'MN'}, 
+  {name: 'Montserrat', code: 'MS'}, 
+  {name: 'Morocco', code: 'MA'}, 
+  {name: 'Mozambique', code: 'MZ'}, 
+  {name: 'Myanmar', code: 'MM'}, 
+  {name: 'Namibia', code: 'NA'}, 
+  {name: 'Nauru', code: 'NR'}, 
+  {name: 'Nepal', code: 'NP'}, 
+  {name: 'Netherlands', code: 'NL'}, 
+  {name: 'Netherlands Antilles', code: 'AN'}, 
+  {name: 'New Caledonia', code: 'NC'}, 
+  {name: 'New Zealand', code: 'NZ'}, 
+  {name: 'Nicaragua', code: 'NI'}, 
+  {name: 'Niger', code: 'NE'}, 
+  {name: 'Nigeria', code: 'NG'}, 
+  {name: 'Niue', code: 'NU'}, 
+  {name: 'Norfolk Island', code: 'NF'}, 
+  {name: 'Northern Mariana Islands', code: 'MP'}, 
+  {name: 'Norway', code: 'NO'}, 
+  {name: 'Oman', code: 'OM'}, 
+  {name: 'Pakistan', code: 'PK'}, 
+  {name: 'Palau', code: 'PW'}, 
+  {name: 'Palestinian Territory, Occupied', code: 'PS'}, 
+  {name: 'Panama', code: 'PA'}, 
+  {name: 'Papua New Guinea', code: 'PG'}, 
+  {name: 'Paraguay', code: 'PY'}, 
+  {name: 'Peru', code: 'PE'}, 
+  {name: 'Philippines', code: 'PH'}, 
+  {name: 'Pitcairn', code: 'PN'}, 
+  {name: 'Poland', code: 'PL'}, 
+  {name: 'Portugal', code: 'PT'}, 
+  {name: 'Puerto Rico', code: 'PR'}, 
+  {name: 'Qatar', code: 'QA'}, 
+  {name: 'Reunion', code: 'RE'}, 
+  {name: 'Romania', code: 'RO'}, 
+  {name: 'Russian Federation', code: 'RU'}, 
+  {name: 'RWANDA', code: 'RW'}, 
+  {name: 'Saint Helena', code: 'SH'}, 
+  {name: 'Saint Kitts and Nevis', code: 'KN'}, 
+  {name: 'Saint Lucia', code: 'LC'}, 
+  {name: 'Saint Pierre and Miquelon', code: 'PM'}, 
+  {name: 'Saint Vincent and the Grenadines', code: 'VC'}, 
+  {name: 'Samoa', code: 'WS'}, 
+  {name: 'San Marino', code: 'SM'}, 
+  {name: 'Sao Tome and Principe', code: 'ST'}, 
+  {name: 'Saudi Arabia', code: 'SA'}, 
+  {name: 'Senegal', code: 'SN'}, 
+  {name: 'Serbia and Montenegro', code: 'CS'}, 
+  {name: 'Seychelles', code: 'SC'}, 
+  {name: 'Sierra Leone', code: 'SL'}, 
+  {name: 'Singapore', code: 'SG'}, 
+  {name: 'Slovakia', code: 'SK'}, 
+  {name: 'Slovenia', code: 'SI'}, 
+  {name: 'Solomon Islands', code: 'SB'}, 
+  {name: 'Somalia', code: 'SO'}, 
+  {name: 'South Africa', code: 'ZA'}, 
+  {name: 'South Georgia and the South Sandwich Islands', code: 'GS'}, 
+  {name: 'Spain', code: 'ES'}, 
+  {name: 'Sri Lanka', code: 'LK'}, 
+  {name: 'Sudan', code: 'SD'}, 
+  {name: 'Suriname', code: 'SR'}, 
+  {name: 'Svalbard and Jan Mayen', code: 'SJ'}, 
+  {name: 'Swaziland', code: 'SZ'}, 
+  {name: 'Sweden', code: 'SE'}, 
+  {name: 'Switzerland', code: 'CH'}, 
+  {name: 'Syrian Arab Republic', code: 'SY'}, 
+  {name: 'Taiwan, Province of China', code: 'TW'}, 
+  {name: 'Tajikistan', code: 'TJ'}, 
+  {name: 'Tanzania, United Republic of', code: 'TZ'}, 
+  {name: 'Thailand', code: 'TH'}, 
+  {name: 'Timor-Leste', code: 'TL'}, 
+  {name: 'Togo', code: 'TG'}, 
+  {name: 'Tokelau', code: 'TK'}, 
+  {name: 'Tonga', code: 'TO'}, 
+  {name: 'Trinidad and Tobago', code: 'TT'}, 
+  {name: 'Tunisia', code: 'TN'}, 
+  {name: 'Turkey', code: 'TR'}, 
+  {name: 'Turkmenistan', code: 'TM'}, 
+  {name: 'Turks and Caicos Islands', code: 'TC'}, 
+  {name: 'Tuvalu', code: 'TV'}, 
+  {name: 'Uganda', code: 'UG'}, 
+  {name: 'Ukraine', code: 'UA'}, 
+  {name: 'United Arab Emirates', code: 'AE'}, 
+  {name: 'United Kingdom', code: 'GB'}, 
+  {name: 'United States', code: 'US'}, 
+  {name: 'United States Minor Outlying Islands', code: 'UM'}, 
+  {name: 'Uruguay', code: 'UY'}, 
+  {name: 'Uzbekistan', code: 'UZ'}, 
+  {name: 'Vanuatu', code: 'VU'}, 
+  {name: 'Venezuela', code: 'VE'}, 
+  {name: 'Viet Nam', code: 'VN'}, 
+  {name: 'Virgin Islands, British', code: 'VG'}, 
+  {name: 'Virgin Islands, U.S.', code: 'VI'}, 
+  {name: 'Wallis and Futuna', code: 'WF'}, 
+  {name: 'Western Sahara', code: 'EH'}, 
+  {name: 'Yemen', code: 'YE'}, 
+  {name: 'Zambia', code: 'ZM'}, 
+  {name: 'Zimbabwe', code: 'ZW'} 
+];
+
 function addEventTouched(elem){
       elem.dispatchEvent(new Event('change', { 'bubbles': true }));
       elem.dispatchEvent(new Event('touch', { 'bubbles': true })); 
@@ -12,7 +259,7 @@ function addEventTouched(elem){
  function submitShipment(){
     setTimeout(function(){ 
          var submitBtn = document.getElementById('submit-button');
-         if (typeof(submitBtn) === 'undefined'){
+         if (typeof(submitBtn) == 'undefined'){
              alert('Кнопка зберегти не знайдена!')
              return;
          }
@@ -24,8 +271,8 @@ function addEventTouched(elem){
   
 //get array   and dom element
 function insertValue(currentRow, containerText) {
-    if (typeof currentRow == undefined){
-   console.log ("currentAddress[2]", currentAddress[2]); return;
+    if (typeof currentRow == 'undefined'){
+   console.log ("currentRow[0] is not set:", currentRow); return;
 }else {
      if (currentRow.length > 1) { 
                var str = ""; 
@@ -67,31 +314,138 @@ function insertValue(currentRow, containerText) {
     div.textContent = JSON.stringify(currentAddress)+ ' current Address = '+j;
     document.body.appendChild(div); 
     //Fill static data
-        var selectType = document.getElementById('packageType');
+    
+//Init fields with __
+var spaces = "..";
+var zipSpaces = "...";
+//var regionText = document.getElementById('region');
+var cityText = document.getElementById('city'); 
+var zipText = document.querySelector("input[data-ng-model='shipment.recipient.address.postcode']");
+var snameText = document.getElementById('surname');
+var nameText = document.getElementById('name');
+var appText =  document.getElementById("apartment"); 
+var phoneText = document.querySelector("input[data-ng-model='shipment.recipient.phoneNumber']");
+var streetText =  document.getElementById("street"); 
+var houseText =  document.getElementById("house"); 
+var country = '..'; 
+var length = currentAddress.length;
+ //country 4 //phone 5
+//Set phone number
+var phone = "..";
+
+if (typeof currentAddress[4] !== "undefined"){
+   phone = currentAddress[4][0];
+   phone = phone.trim();
+ 
+    if (phone.indexOf("Phone :") != -1){
+       phone = phone.replace("Phone :", "");
+       currentAddress[4][0] = phone; 
+       console.log('phone', phone);
+    } 
+    phoneText.value = phone; 
+    addEventTouched(phoneText);
+}
+ 
+
+//add spaces in unnesessary values
+
+ 
+ 
+// appText.value = spaces; !not needed for now
+ //regionText.value = spaces;
+ houseText.value = spaces;
+   
+//addEventTouched(appText); !not needed for now
+ 
+//addEventTouched(regionText); !not needed for now
+ 
+zipText.value = zipSpaces;
+addEventTouched(zipText);
+var currentRow  = currentAddress[2]; 
+
+insertValue(currentRow, cityText); 
+addEventTouched(cityText);
+
+currentRow  = currentAddress[1]; 
+insertValue(currentRow, streetText);
+addEventTouched(streetText);
+
+ 
+
+currentRow = currentAddress[0];
+insertValue(currentRow, nameText)
+snameText.value = spaces;
+//emulate client clicks
+addEventTouched(nameText);
+addEventTouched(snameText); 
+        
+//check.
+  
+if (typeof currentAddress[3] !== "undefined"){
+    country = currentAddress[3][0]; 
+    country = country.toLowerCase();
+    console.log('country', country);
+};
+country = country.trim(); 
+var countryCode = "";
+//get value
+for (var i=0; i<countryList.length; i++){
+    var currCountry = countryList[i];
+   if (country == (currCountry.name).toLowerCase() || country == (currCountry.code).toLowerCase()){
+       countryCode = currCountry.code;
+       break;
+       console.log("country code", countryCode);
+   } 
+}
+ var selectCountry = document.getElementById('country');
+
+//if country code not found
+console.log("!country code", countryCode);
+
+//check current country, select option.
+if(countryCode == ""){
+    selectCountry.options[0].selected = true;
+}else {
+   for (var n=1; n < selectCountry.options.length; n++){    
+    var currOption = selectCountry.options[n]; 
+    if (countryCode == currOption.value){
+        console.log('found country code', currOption.value); 
+        currOption.selected = true; 
+        console.log('fcurrOption', currOption);
+        break;
+    }
+}   
+
+}
+ addEventTouched(selectCountry); 
+
+//Static additional fields
+
+var selectType = document.getElementById('packageType');
         selectType.options[1].selected = true; 
         addEventTouched(selectType);
        // selectType.dispatchEvent(new Event('change', { 'bubbles': true }));
-    var selectSposob = document.getElementById('avia');
-    selectSposob.options[1].selected = true; 
-    setTimeout(function(){ 
-        
+        var selectSposob = document.getElementById('avia');
+        selectSposob.options[1].selected = true; 
+        setTimeout(function(){ 
+
        
         //selectSposob.dispatchEvent(new Event('change', { 'bubbles': true }));
         addEventTouched(selectSposob);
         
             
-            var quantity =  document.getElementById('parcelItemsQuantity');
-            quantity.value = staticData.quantity;
-            addEventTouched(quantity);
-            var parcelItemsValue =  document.getElementById('parcelItemsValue');
-            parcelItemsValue.value = staticData.parcelItemsValue; 
-            addEventTouched(parcelItemsValue);
-            var parcelItemsWeight =  document.getElementById('parcelItemsWeight');
-            parcelItemsWeight.value = staticData.netto; 
-            addEventTouched(parcelItemsWeight);
-            var nameV =  document.getElementById('parcelItemsName');
-            nameV.value = staticData.nameV;
-            addEventTouched(nameV);
+        var quantity =  document.getElementById('parcelItemsQuantity');
+        quantity.value = staticData.quantity;
+        addEventTouched(quantity);
+        var parcelItemsValue =  document.getElementById('parcelItemsValue');
+        parcelItemsValue.value = staticData.parcelItemsValue; 
+        addEventTouched(parcelItemsValue);
+        var parcelItemsWeight =  document.getElementById('parcelItemsWeight');
+        parcelItemsWeight.value = staticData.netto; 
+        addEventTouched(parcelItemsWeight);
+        var nameV =  document.getElementById('parcelItemsName');
+        nameV.value = staticData.nameV;
+        addEventTouched(nameV);
         
         var brutto =  document.getElementById('weight');
         brutto.value = staticData.brutto;
@@ -113,120 +467,6 @@ function insertValue(currentRow, containerText) {
         addEventTouched(categoryType);
         
         //categoryType.dispatchEvent(new Event('change', { 'bubbles': true })); 
-        // submitShipment();
+         submitShipment();
        
-    }, 600);
- 
-var country = '..';
-var phone = '..';
-var length = currentAddress.length;
- 
-//Check country or phone
-var check = currentAddress[length - 1][0];
-check = check.trim();
-console.log('check country or phone', check);
-if (check.indexOf("Phone :") != -1){
-   check = check.replace("Phone :", "");
-   currentAddress[length - 1][0] = check;
-   phone = check;
-   //console.log('phone', check);
-   // alert(phone);
-} 
-var isCountry = /^(\s?\.?[a-zA-Z]+)+$/.test(check);
-var regexHasDigit = /\d/;
-if (isCountry){
-    country = check; 
-} else {
-    phone = check; 
-    country = currentAddress[length - 2][0]; 
-}  
-  
-  
- 
- 
-country = country.toLowerCase();
-console.log('country', country);
-
-
-//Init fields with __
-var spaces = "..";
-var zipSpaces = "...";
-var regionText = document.getElementById('region');
-var cityText = document.getElementById('city'); 
-var zipText = document.querySelector("input[data-ng-model='shipment.recipient.address.postcode']");
-var snameText = document.getElementById('surname');
-var nameText = document.getElementById('name');
-var appText =  document.getElementById("apartment"); 
-var phoneText = document.querySelector("input[data-ng-model='shipment.recipient.phoneNumber']");
-var streetText =  document.getElementById("street"); 
-var houseText =  document.getElementById("house"); 
-//add spaces in unnesessary values
- phoneText.value = phone;  
-// appText.value = spaces;
- regionText.value = spaces;
- houseText.value = spaces;
-   
-addEventTouched(appText);
-addEventTouched(phoneText); 
-addEventTouched(regionText);
-//nameText.value = spaces;
-//snameText.value = spaces;
-//regionText.value = spaces;
-//cityText.value = spaces;
-zipText.value = zipSpaces;
-addEventTouched(zipText);
-var currentRow  = currentAddress[2]; 
-
-insertValue(currentRow, cityText); 
-addEventTouched(cityText);
-
-currentRow  = currentAddress[1]; 
-insertValue(currentRow, streetText);
-addEventTouched(streetText);
-
- 
-
-currentRow = currentAddress[0];
-insertValue(currentRow, nameText)
-snameText.value = spaces;
-//emulate client clicks
-addEventTouched(nameText);
-addEventTouched(snameText); 
-        
-
-//heck current country, select option.
- var selectCountry = document.getElementById('country');
- //selectCountry.options[197].selected = true; 
-   //country get and check 
-  if (country.indexOf("us") !=-1 || country.indexOf("states") != -1 || country.indexOf("u.s")!=-1){
-    selectCountry.options[197].selected = true; 
-}
-else if (country.indexOf("germany") != -1 || country.indexOf("deutschland") != -1){
-    selectCountry.options[144].selected = true; 
-}else if (country.indexOf("portugal") != -1 || country.indexOf("portugal") != -1){
-    selectCountry.options[168].selected = true; 
-}
-else if (country.indexOf("kingdom") != -1 || country.indexOf("britain") != -1){
-    selectCountry.options[38].selected = true; 
-}else if (country.indexOf("mexico") != -1 || country.indexOf("mexica") != -1){
-     selectCountry.options[131].selected = true;  
-} else if (country.indexOf("japan") != -1 || country.indexOf("日本") != -1){
-    selectCountry.options[236].selected = true; 
-} 
-else if (country.indexOf("canada") != -1){
-    selectCountry.options[90].selected = true; 
-}
- 
- addEventTouched(selectCountry);
-                  
- //selectCountry.dispatchEvent(new Event('change', { 'bubbles': true }));
-  
- 
-    //GET last idx if digit: phone
-    //else country
-    //console.log('inside timeout', j, arrayAddr);
-    //Debug Data:
-
-     //define arrayAddr
-      
-     
+    }, 400);
